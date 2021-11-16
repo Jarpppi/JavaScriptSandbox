b2c153b52efa0b78b2821875a1b90746b9b3bc08
Opilane TTHK <opilane@tthk.ee>
2021-11-16 11:40:36 +0200

    mardo

---
+++ colors.js
@@ -0,0 +1,15 @@
+//console.log("hello from script");
+const myButton = document.querySelector('button');
+const myBox = document.querySelector('Box');
+const colors = ['red', 'green', 'blue', 'yellow'];
+//console.log(myButton);
+
+myButton.addEventListener('click', changeColor);
+
+function changeColor() {
+    let rnd = Math.floor(Math.random() * colors.lenght();
+    console.log(rnd);
+    myBox.
+
+
+}
