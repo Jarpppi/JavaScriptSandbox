29c6d45fe28a4c1e7156f86414dcf634067a67a3
Opilane TTHK <opilane@tthk.ee>
2021-11-17 14:59:48 +0200

    mardo

--- colors.js
+++ colors.js
@@ -3,13 +3,28 @@ const myButton = document.querySelector('button');
 const myBox = document.querySelector('Box');
 const colors = ['red', 'green', 'blue', 'yellow'];
 //console.log(myButton);
+let randomValues = [];
+let rnd = Math.floor(Math.random() * colors.lenght();
+    console.log(rnd);
+    myBox
+
+let name = "Harry Potter";
+console.log(´Hello, $(name)´);
 
 myButton.addEventListener('click', changeColor);
 
 function changeColor() {
-    let rnd = Math.floor(Math.random() * colors.lenght();
-    console.log(rnd);
-    myBox.
+   if(randomValues.length === 0) {
+       for(let i = 0; i < colors.length; i++ ) {
+           randomValues.push(i);
+           console.log("$(i) added");
+       }
+   }
+   let randomIndex = Math.floor(Math.random() * randomValues.length);
+   let indexFromMyarray = randomValues[randomIndex];
+   randomValues.splice(randomIndex, 1);
+
+   myBox.style.backgroundColor = colors(indexFromMyarray);
 
 
 }
